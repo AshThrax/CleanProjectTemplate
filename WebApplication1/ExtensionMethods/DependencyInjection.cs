3dc3aa225ec74397eb30dbbfdc13161eab6e81e7
Quentin Courcelles <78923290+AshThrax@users.noreply.github.com>
2024-10-15 18:01:32 +0200

    Add files via upload

---
+++ WebApplication1/ExtensionMethods/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtensionMethods.Service
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplication();
+            services.AddInfrastructure(configuration);
+            return services;
+        }
+    }
+}
---
+++ WebApplication1/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Domain.Entity;
+global using Application.Common;
+global using Infrastructure.Persistence;
+global using Infrastructure;
+global using Application;
